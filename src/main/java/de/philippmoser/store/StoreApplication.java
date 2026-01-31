e9f562963c117ae2b2077df6c84fbda47358fbda
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2026-01-31 18:25:10 +0100

    Setup Lombock with setter, getter and constructors

--- src/main/java/de/philippmoser/store/StoreApplication.java
+++ src/main/java/de/philippmoser/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package de.philippmoser.store;
 
+import de.philippmoser.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,14 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         var context = SpringApplication.run(StoreApplication.class, args);
+
+        var user1 = new User(1L, "John", "john@example.com", "password");
+
+        var user2 = new User();
+
+        user2.setName("John");
+        user2.setEmail("john@example.com");
+        user2.setPassword("password");
     }
 
 }
