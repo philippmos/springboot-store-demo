baec33774fa851fe02b85288028b6f682124e57e
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2026-02-08 11:34:37 +0100

    Implement Query Parameter

--- src/main/java/de/philippmos/store/controllers/UserController.java
+++ src/main/java/de/philippmos/store/controllers/UserController.java
@@ -4,13 +4,12 @@ import de.philippmos.store.dtos.UserDto;
 import de.philippmos.store.mappers.UserMapper;
 import de.philippmos.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -20,9 +19,15 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sort
+    ) {
+        if (!Set.of("name", "email").contains(sort)) {
+            sort = "name";
+        }
+
         return userRepository
-                .findAll()
+                .findAll(Sort.by(sort).descending())
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
