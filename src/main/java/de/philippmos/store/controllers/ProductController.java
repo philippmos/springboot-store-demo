48c6d5c4e2a36c8e47d756f75fff2630e2d80359
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2026-02-15 08:14:57 +0100

    Setup further Products Endpoints

--- src/main/java/de/philippmos/store/controllers/ProductController.java
+++ src/main/java/de/philippmos/store/controllers/ProductController.java
@@ -1,13 +1,12 @@
 package de.philippmos.store.controllers;
 
-
 import de.philippmos.store.dtos.ProductDto;
+import de.philippmos.store.entities.Product;
 import de.philippmos.store.mappers.ProductMapper;
 import de.philippmos.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +18,29 @@ public class ProductController {
     private final ProductMapper productMapper;
 
     @GetMapping
-    public List<ProductDto> getAll() {
-        return productRepository.findAll().stream().map(productMapper::toDto).toList();
+    public List<ProductDto> getAll(
+            @RequestParam(name = "categoryId", required = false) Byte categoryId
+    ) {
+        List<Product> products;
+
+        if (categoryId != null) {
+            products = productRepository.findByCategoryId(categoryId);
+        }
+        else {
+            products = productRepository.findAllWithCategory();
+        }
+
+        return products.stream().map(productMapper::toDto).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> get(@PathVariable Long id){
+        var user =  productRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(productMapper.toDto(user));
     }
 }
